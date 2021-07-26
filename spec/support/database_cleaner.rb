cb7dc569fc50640298e45bcf543e1b5109e58f3f
ahnemui <clh.oneil@gmail.com>
2021-07-26 14:48:04 +1000

    rspec test setup done

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,26 @@
+# rspec configurations
+# configurations for database cleaner 
+
+Rspec.configure do |config|
+    # want to set up truncation cleaning strategy nefore test suite runs
+    config.before(:suite) do
+        DatabaseCleaner.clean_with(:truncation)
+    end
+
+    # =sets transaction stretegy before each test
+    # create this because we set transactional_fixtures to false in rails_helper.rb
+    # this means that instead of letting rails handle this we want the database cleaner to handle this
+    config.before(:each) do 
+        DatabaseCleaner.strategy = :transaction
+    end
+
+    # want to start the database cleaner gem before each test
+    config.before(:each) do
+        DatabaseCleaner.start
+    end
+
+    # want to do a database clean up after each test
+    config.after(:each) do
+        DatabaseCleaner.clean
+    end
+end
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,4 @@
+Rspec.configure do |config|
+    # want to use the factory bot syntaxes in all of my tests
+    config.include FactoryBot::Syntax::Methods
+end
