5a667f026761c062a8c0d16bc92589f241e05127
ahnemui <clh.oneil@gmail.com>
2021-07-27 12:53:35 +1000

    validations for the addresses model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :street_no, presence: true
+  validates :street, presence: true
+  validates :suburb, presence: true
+  validates :postcode, presence: true, length: { is: 4 }, format: { with: /3\d{3}/,
+    message: "error: RENT-Y-FiT is currently only avaiolable to Victorians" }
 end
