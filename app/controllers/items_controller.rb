df55bd6569fe440330938888a2eae86496a9fa64
ahnemui <clh.oneil@gmail.com>
2021-07-29 21:50:52 +1000

    preloaded users table inside items#index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
 
   # GET /items or /items.json
   def index
-    @items = Item.all
+    @items = Item.all.preload(:user)
   end
 
   # GET /items/1 or /items/1.json
