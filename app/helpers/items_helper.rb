000fc78ab283a4c752f9724965b5bc4776d29bfd
ahnemui <clh.oneil@gmail.com>
2021-08-02 10:35:51 +1000

    uploaded default image

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -4,4 +4,10 @@ module ItemsHelper
         dollars = cents.to_f / 100
         '%.2f' % dollars
     end
+
+    def product_image(item)
+        return item.picture if item.picture.attached?
+        # if no image is attached to an item return a default
+        return 'RENT-Y-FiT_Logo.png'
+    end
 end
