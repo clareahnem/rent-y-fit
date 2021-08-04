5eaf7736eee5281078cc45125c049b8c914dcee6
ahnemui <clh.oneil@gmail.com>
2021-08-05 01:29:46 +1000

    created switch_availability route and method to switch item availability

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :items do
     resources :bookings, only: [:create, :show, :update, :edit]
+    member do
+      get :switch_availability
+    end
   end
   resources :bookings, only: [:index, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get '/about', to: 'pages#about', as: 'about'
-
+  get '/dashboard', to: 'pages#dashboard', as:'dashboard'
   
   
 
