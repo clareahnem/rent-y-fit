6dd2b3a1cad3e35d628c3ef44be7a12f992e16b7
ahnemui <clh.oneil@gmail.com>
2021-08-06 18:20:23 +1000

    items-bookings show page done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,8 @@
 class BookingsController < ApplicationController
-    before_action :set_item, only: [:create]
-    before_action :set_booking, only: [:destroy, :edit, :update]
+    before_action :set_item, only: [:create, :show]
+    before_action :set_booking, only: [:show, :destroy, :edit, :update]
     def index
+        # =========displays "your bookings" page========
         # want to show all items that pending user's booking request
         @pendingitems = Booking.where(status: "pending", requesting_user_id: current_user.id).eager_load(:item)
         
@@ -13,17 +14,19 @@ class BookingsController < ApplicationController
     end
 
     def create
-    # booking request form submission
-      @booking = @item.bookings.new(booking_params)
-      @booking.requesting_user_id = current_user.id
-      @booking.save
-      redirect_to bookings_path, notice: "Your booking request has been sent to the owner"
+        # =========booking request form submission. form displayed in item's show page ============
+        @booking = @item.bookings.new(booking_params)
+        @booking.requesting_user_id = current_user.id
+        @booking.save
+        redirect_to bookings_path, notice: "Your booking request has been sent to the owner"
     end
 
     def show
+        #==========shows item's booking. Requesting user can proceed to payment through here==========
     end
 
     def update
+        # =========updates booking status through dashboard. changeable by owner of item ===========
         @booking.update_column(:status, params[:status])
         redirect_back fallback_location: dashboard_path, notice:"Booking status was successfully updated"
     end
@@ -32,6 +35,7 @@ class BookingsController < ApplicationController
     end
 
     def destroy
+        # ==========destroys booking request. can be deleted by requesting user =========
         @booking.destroy
         redirect_to bookings_path, notice: "Booking request was successfully destroyed"
     end
