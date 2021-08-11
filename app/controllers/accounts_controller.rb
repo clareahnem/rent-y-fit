b3691ee138b3782c0e1d805c63394639c25a8d32
ahnemui <clh.oneil@gmail.com>
2021-08-11 22:59:49 +1000

    created show page for account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < ApplicationController
+    before_action :set_variables
+    before_action :authenticate_user!
 
     def show
         # page that shows current user's account information (email, username, addresses)
@@ -8,5 +10,11 @@ class AccountsController < ApplicationController
     end
     def update
         # update address info
+    end
+
+    private
+    def set_variables
+        @user = User.find(params[:id]) 
+        @address = Address.find_by(user_id: current_user.id)
     end
 end
