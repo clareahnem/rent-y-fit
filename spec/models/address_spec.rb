f71de2a70cdfda3220ea6c71d21c8692184df3a9
ahnemui <clh.oneil@gmail.com>
2021-08-12 12:38:45 +1000

    rspec validations added

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe Address, type: :model do
       expect(address).to_not be_valid
     end
 
+    it 'is not valid without a street number' do
+      address = build(:address, street_no: nil)
+      expect(address).to_not be_valid
+    end
+
+    it 'is not valid without a street name' do
+      address = build(:address, street: nil)
+      expect(address).to_not be_valid
+    end
+
+    it 'is not valid without a suburb' do
+      address = build(:address, suburb: nil)
+      expect(address).to_not be_valid
+    end
+
     it "is not valid outside of victoria" do
       address = build(:address, postcode: "4000")
       expect(address).to_not be_valid
